145091f1e30518cc098e1c26553eb214ff510f10
MITCHELLAL21 <malstromrd22@gg.edu>
2025-03-06 18:27:20 -0500

    Tired to start the recursive algorithm but need to work with graddle so I can test as I go

--- src/main/java/software/engineering/main/AutoScheduler.java
+++ src/main/java/software/engineering/main/AutoScheduler.java
@@ -27,4 +27,67 @@ public class AutoScheduler {
     public void removeFromCourseList(Timeblock sec) {
         this.CourseList.remove(sec);
     }
+
+    public ArrayList<Schedule> getSchedulePossibilties() {
+        return SchedulePossibilties;
+    }
+
+    public void generatePossibleSchedules(){ // might add filters to the param
+        //1) grab first section/timeblock
+
+        //2) see if there is mulitple times for that section
+        // if so then split, make a schedule for each section seperatly
+
+        //3) split for everyother section seperately (adding each class that is left
+        // to possilble scheduled as if it was added second)
+
+        //4) repeat 3 till untill no more sections
+
+        //5) repeat 1-4 for every course
+
+
+        Schedule tempSch = new Schedule();
+        this.SchedulePossibilties = genPossibleSchedulesHelper(SchedulePossibilties,CourseList,tempSch);
+        // now we should have a list of schedules to work with
+
+        /*TODO: Filter through the schedules that are most benifical and those that are not
+        / Factors:
+        /   How early the first class
+        /   How many classes are fit into one schedule
+        /   is there time for lunch
+        /   do they end before practice
+        /   are the classes you really wanted to take in the schedule (might be a later addition)
+        */
+    }
+
+    public ArrayList<Schedule> genPossibleSchedulesHelper(ArrayList<Schedule> SchedulePossibilties, ArrayList<Timeblock> coursesLeft, Schedule curSec) {
+        // should be recursive I think it can be done
+        // base case is when there is no more courses left in the course left
+
+        //Could this be void since we are adding the schedule to a list that is being passed in
+
+        //Base Case
+        if(coursesLeft.isEmpty()) {
+            SchedulePossibilties.add(curSec);
+            return SchedulePossibilties;
+        }
+
+        // go through all courses and add them
+        for(int i = 0; i < coursesLeft.size(); i++) {
+            // make a new schedule for the split
+
+            //TODO: Add rules to handle conflicts and
+            // so timeblocks always take precidence over sections
+            // also will have call if there is more than one time section
+            curSec.addTimeblock(coursesLeft.get(i));
+
+            //placeholder courses left without the course we just removed
+            ArrayList<Timeblock> temp = new ArrayList<>(coursesLeft); //Might Not be a deep Copy IDK
+            temp.remove(coursesLeft.get(i));
+
+            genPossibleSchedulesHelper(SchedulePossibilties,temp,curSec);
+        }
+    }
+
+
 }
