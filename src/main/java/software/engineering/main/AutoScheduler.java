a3e36e6df5557f42f716b175a769ceab25e42695
MITCHELLAL21 <malstromrd22@gg.edu>
2025-03-06 17:30:39 -0500

    Possible to add and remove timeblock objects including sections

--- src/main/java/software/engineering/main/AutoScheduler.java
+++ src/main/java/software/engineering/main/AutoScheduler.java
@@ -3,18 +3,28 @@ package software.engineering.main;
 import java.util.ArrayList;
 
 public class AutoScheduler {
-    private ArrayList SchedulePossibilties;
+    private ArrayList<Schedule> SchedulePossibilties;
     //All courses you would want to take regardless of conflicts
-    private ArrayList CourseList; //All courses you would want to take regardless
+    private ArrayList<Timeblock> CourseList;
 
     //Constructor given no input
     public AutoScheduler() {
-
+        this.SchedulePossibilties = new ArrayList<>();
+        this.CourseList = new ArrayList<>();
     }
 
     //Constructor given a schedule object
     public AutoScheduler(Schedule Sch) {
+        this.SchedulePossibilties = new ArrayList<>();
+        this.CourseList = new ArrayList<>();
 
+        //make a copy of
     }
 
+    public void addToCourseList(Timeblock sec) {
+        this.CourseList.add(sec);
+    }
+    public void removeFromCourseList(Timeblock sec) {
+        this.CourseList.remove(sec);
+    }
 }
