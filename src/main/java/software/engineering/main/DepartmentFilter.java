74aec477bbe9ed627a8f75da5c27c5eb4fb9d69b
LegoBuilder370 <cjkobulnicky@gmail.com>
2025-03-12 14:54:45 -0400

    Filters are complete, but not tested Search has filters working, but not the original search method This code should not affect any other code

--- src/main/java/software/engineering/main/DepartmentFilter.java
+++ src/main/java/software/engineering/main/DepartmentFilter.java
@@ -12,6 +12,14 @@ public class DepartmentFilter extends Filter {
 
     @Override
     public ArrayList<Section> filter(ArrayList<Section> sectionList) {
-        return null;
+        ArrayList<Section> returnList = new ArrayList<Section>();
+
+        for (int i = 0; i < sectionList.size(); i++) {
+            if (sectionList.get(i).getDepartment() == department) {
+                returnList.add(sectionList.get(i));
+            }
+        }
+
+        return returnList;
     }
 }
--- src/main/java/software/engineering/main/Search.java
+++ src/main/java/software/engineering/main/Search.java
@@ -3,22 +3,35 @@ import java.util.ArrayList;
 
 public class Search {
 
+    protected ArrayList<Section> fullList;
     protected ArrayList<Section> lastSearch;
     protected ArrayList<Filter> filtersInUse;
 
-    public Search() {
-
+    public Search(ArrayList<Section> list) {
+        fullList = new ArrayList<>(list);
+        lastSearch = new ArrayList<>();
+        filtersInUse = new ArrayList<>();
     }
 
     public ArrayList<Section> courseSearch(String text) {
         return null;
     }
 
+    // I think this is done but I'm not sure - LegoBuilder
     public ArrayList<Section> applyFilters() {
-        return null;
+        ArrayList<Section> results = new ArrayList<Section>();
+        results.addAll(lastSearch);
+
+        for (int i = 0; i < filtersInUse.size(); i++) {
+            results = filtersInUse.get(i).filter(results);
+        }
+
+        lastSearch.clear();
+        lastSearch.addAll(results);
+        return results;
     }
 
     public void addFilter(Filter filter) {
-
+        filtersInUse.add(filter);
     }
 }
