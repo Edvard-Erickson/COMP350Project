73b8ae7a0fa0bd1faa156afcc74466b046604850
LegoBuilder370 <cjkobulnicky@gmail.com>
2025-03-15 20:37:05 -0400

    minor updates to timeblock

--- src/main/java/software/engineering/main/Timeblock.java
+++ src/main/java/software/engineering/main/Timeblock.java
@@ -1,6 +1,7 @@
 package software.engineering.main;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -35,7 +36,6 @@ public class Timeblock {
         return name;
     }
 
-    // TODO: this needs to be tested
     // returns true if the timeblocks overlap at any point, false otherwise
     protected boolean conflictsWith(Timeblock other) {
         Iterator timesI = times.keySet().iterator();
@@ -55,8 +55,22 @@ public class Timeblock {
                     String otherStart = timeSlots[1];
                     String otherEnd = timeSlots[1];
 
-                    if ((start.compareTo(otherStart) <= 0) && (end.compareTo(otherStart) > 0) ||
-                            (start.compareTo(otherEnd) <= 0) && (end.compareTo(otherEnd) > 0)) {
+                    ArrayList<String> sCon = new ArrayList<String>();
+
+                    sCon.add(start);
+                    sCon.add(end);
+                    sCon.add(otherStart);
+                    sCon.add(otherEnd);
+                    for (int i = 0; i < 4; i++) {
+                        // TODO: find what length 10:00 is stored as and change the 5 below to that number
+                        if (sCon.get(i).length() < 5) {
+                            String s = (String) sCon.get(i);
+                            sCon.set(i, "0" + s);
+                        }
+                    }
+
+                    if (((start.compareTo(otherStart) <= 0) && (end.compareTo(otherStart) > 0)) ||
+                            ((start.compareTo(otherEnd) <= 0) && (end.compareTo(otherEnd) > 0))) {
                         return true;
                     }
                 }
