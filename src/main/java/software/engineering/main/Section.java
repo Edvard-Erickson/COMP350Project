1d8dbc146fcb51007f3a180c31e396a2967c94e3
Edvard-Erickson <ericksoneg22@gcc.edu>
2025-03-18 12:21:39 -0400

    added semester to database and section.

--- src/main/java/software/engineering/main/Section.java
+++ src/main/java/software/engineering/main/Section.java
@@ -5,27 +5,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Section extends Timeblock{
-    protected ArrayList<String> prereq; // a list of courses saved as department course code (ie. HUMA220)
     protected String department; // Four Chars (ie. HUMA COMP SSFT)
     protected int courseCode; // three digit code such as (300 200 432)
     protected char section; // a char that repersents each section (ie A B L)
+    private String semester;
     private String professor; //Full name of the professor
 
-    protected Section(String department, int courseCode, char section, String courseName, String professor, HashMap<String, String[]> times) {
+    protected Section(String department, int courseCode, char section, String courseName, String professor, String semester, HashMap<String, String[]> times) {
         //Might Need to flesh out
         super(times, courseName);
         this.department = department;
         this.courseCode = courseCode;
         this.section = section;
         this.professor = professor;
+        this.semester = semester;
 
-        //each
-        prereq = new ArrayList<>();
-    }
-
-    protected void setPrereqs(ArrayList<String> prereqs) {
-        //prereqs saved as course code (i.e. COMP222)
-        this.prereq = prereqs;
     }
 
     protected String getDepartment() {
@@ -46,4 +40,9 @@ public class Section extends Timeblock{
 
     protected char getSection(){ return this.section;}
 
+    protected String getSemester(){ return this.semester;}
+
+    protected boolean sectionOf(Section other) {
+        return this.department.equals(other.getDepartment()) && this.courseCode == other.getCourseCode();
+    }
 }
