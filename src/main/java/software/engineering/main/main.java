44c432d8d258a5d5cd53df59796eee0bace75b51
lsuorsa <suorsaLE22@gcc.edu>
2025-03-18 12:41:16 -0400

    filters should work now

--- src/main/java/software/engineering/main/main.java
+++ src/main/java/software/engineering/main/main.java
@@ -14,6 +14,7 @@ public class main {
     }
 
     public static void main(String[] args) {
+        /*
         System.out.println("hello");
         System.out.println("Eddie is here");
 
@@ -98,5 +99,7 @@ public class main {
         Gson gson = new Gson();
 
         System.out.println(gson.toJson(a));
+
+         */
     }
 }
